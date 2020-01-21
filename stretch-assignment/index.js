b6500ae2744ff74230b983d55573e73b71ee4373
DrewCDoll <drwcdoll@gmail.com>
2020-01-21 18:58:13 -0500

    stretch project base completed

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,11 +1,54 @@
 window.onload = function () {
-    const blocks = document.querySelectorAll('.block');
-    blocks.forEach((item, index) => {
-        item.addEventListener('click', (e) => {
-            e.stopPropagation();
-            let tempColor = blocks[0];
-            
-        }
+    const bRed = document.querySelector('.block--red')
+    const bBlue = document.querySelector('.block--blue')
+    const bGreen = document.querySelector('.block--green')
+    const bPink = document.querySelector('.block--pink')
+    const bGray = document.querySelector('.block--gray')
+    let myTimer
+
+    let bArray = [
+        bRed,
+        bBlue,
+        bGreen,
+        bPink,
+        bGray
+    ]
+
+    function trvlTimer (block) {
+        const grow = "10px"
+        block.style.width = block.style.width + grow
+    } 
+
+
+    const bLocation = function (block) {
+        let tempOrd = 10
+        bArray.forEach((item) => {
+            if (parseInt(item.style.order) < tempOrd) {
+                tempOrd = parseInt(item.style.order)
+            }
+        })
+        block.style.order = tempOrd - 1 
+    }
+
+
+   
+    bArray.forEach(function(block, index) {
+        block.style.order = index;
+        block.addEventListener('click', e => {
+            e.preventDefault()
+            e.stopPropagation()
+            bLocation(block)
+        })
+        block.addEventListener('mousedown', e => {
+            e.preventDefault()
+            e.stopPropagation()
+            block.style.width = '100px'
+            myTimer = setInterval(trvlTimer(block), 10)
+        } )
+    })
+
+
+
 
 
 }
